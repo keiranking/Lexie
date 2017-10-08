7c5064b91b1d5423fe9e69b8a831ea46a53f7cd3
keiranking <me@keiranking.com>
2017-10-08 14:36:55 -0400

    New add function.
    Also:
    Can iterate Wordlist object directly (eg, wl[3]).

--- lexie.py
+++ lexie.py
@@ -5,59 +5,83 @@ import operator
 import math
 
 MINIMUM_WORD_LENGTH = 3
-MAXIMUM_WORD_LENGTH = 3
-MINIMUM_WORD_FREQUENCY = 2  # Minimum crossword appearances to qualify for wordlist
+MAXIMUM_WORD_LENGTH = 15
+MINIMUM_XW_APPEARANCES = 2  # Minimum crossword appearances to qualify for wordlist
 MINIMUM_WORD_SCORE = 60
 
 class Wordlist(object):
-    def __init__(self, file): # takes file containing one entry per line and nothing else
-        self.words = [{}, {}, {}]
-        self._temp_words = []
+    def __init__(self, file=False): # takes file containing one entry per line and nothing else
+        self.words = []
+        if not file:
+            return
+
         try:
             with open(file, "r") as raw:
                 print("Importing " + file + "...")
-                raw_list = raw.read().split("\n")
-                for new_word in raw_list:
-                    if new_word:
-                        new_word = new_word.strip().upper()
-                        while len(new_word) >= len(self._temp_words):
-                            self._temp_words.append([])
-                            # self.words.append({})
-                        if MAXIMUM_WORD_LENGTH >= len(new_word) >= MINIMUM_WORD_LENGTH\
-                                and new_word not in self._temp_words[len(new_word)]:
-                            self._temp_words[len(new_word)].append(new_word)
-
-                for i in range(3, len(self._temp_words)):
-                    self.words.append(self.score(self._temp_words[i]))
-                print("Wordlist imported.")
-
+                self.add(raw.read().split("\n"))
         except FileNotFoundError:
             print("Could not open '" + file + "'.")
             return
 
-    def score(self, words):
+    def __iter__(self):
+        return self.words
+
+    def __getitem__(self, i):
+        return self.words[i]
+
+    def add(self, list):
+        new_words = []
+        count = 0
+        for new_word in list:
+            if new_word:
+                new_word = new_word.strip().upper()
+                l = len(new_word.replace(" ", ""))
+                while l >= len(new_words):
+                    new_words.append([])
+                while l >= len(self.words):
+                    self.words.append({})
+                if MAXIMUM_WORD_LENGTH >= l >= MINIMUM_WORD_LENGTH \
+                        and new_word not in new_words[l]:
+                    new_words[l].append(new_word)
+                    count += 1
+                # print(new_word, new_words[len(new_word)])
+                # print(new_word, self.words[len(new_word)])
+
+        for i in range(3, len(new_words)):
+            print("Adding " + str(i) + "-letter words...")
+            merged_words = {**self.words[i], **self.score(new_words[i])}
+            self.words.pop(i)
+            self.words.insert(i, merged_words)
+        print(str(count) + " words added.")
+
+    def score(self, list):
         scored_wl = {}
-        for word in words:
+        if not list:
+            return scored_wl
+        for word in list:
             # print(word)
             if word:
-                xw_page = BS(req.get("http://crosswordtracker.com/search/?answer=" + word.lower()).content,
+                # Crossword score is all-time appearances in major crosswords
+                candidate = word.upper().replace(" ", "")
+                xw_query = word.lower().replace(" ", "")
+                xw_scrape = BS(req.get("http://crosswordtracker.com/search/?answer=" + xw_query).content,
                              "html.parser")
-                cu_page = BS(req.get("https://www.google.com/search?q=" + word.lower() + "&tbs=qdr:y").content,
-                             "html.parser")
-
-                # Crossword score is all-time appearances in major crosswords (from Crossword Tracker, max=20)
                 try:
-                    xw_score = int(re.search(r'\d+', str(xw_page.find_all(string=re.compile("we have spotted"))))[0])
+                    xw_score = int(re.search(r'\d+', str(xw_scrape.find_all(string=re.compile("we have spotted"))))[0])
                 except TypeError:
                     xw_score = 0
-                # Culture score is Google results dated within the past year, in millions
-                cu_score = int(
-                    re.search(r'\d+(,?\d*)*', str(cu_page.find(id="resultStats").string))[0].replace(",", "")) / 1000000
-                if not xw_score or xw_score < MINIMUM_WORD_FREQUENCY:  # Disqualify non-crossword words
-                    scored_wl[word] = 0
+                if xw_score < MINIMUM_XW_APPEARANCES:  # Disqualify non-crossword words
+                    scored_wl[candidate] = 0
                 else:
-                    scored_wl[word] = round(cu_score * xw_score)
-                print(word, scored_wl[word])
+                    # Culture score is Google search results from the past year, in millions
+                    cu_query = word.lower().replace(" ", "+")
+                    cu_scrape = BS(req.get("https://www.google.com/search?q=" + cu_query + "&tbs=qdr:y").content,
+                                 "html.parser")
+                    cu_score = int(
+                        re.search(r'\d+(,?\d*)*', str(cu_scrape.find(id="resultStats").string))[0].replace(",", "")) / 1000000
+
+                    scored_wl[candidate] = round(cu_score * xw_score)
+                print(candidate, scored_wl[candidate])
         return scored_wl
 
     def write(self, filepath, scored=False, sorted_by="keys"):
@@ -75,9 +99,9 @@ class Wordlist(object):
                         doc.write("\t" + str(value))
                     doc.write("\n")
                 doc.write("\n")
-                print("Wrote " + str(i) + "-letter words to file")
+                print("Wrote " + str(i) + "-letter words to " + str(filepath))
             doc.close()
-            print("Finished writing to file")
+            print("Finished writing to " + str(filepath))
         except FileNotFoundError:
             print("Could not open '" + file + "'.")
             return
@@ -88,5 +112,11 @@ class Wordlist(object):
 
 # print(score(["THE"]))
 
-wl = Wordlist("wl-test.txt")
-wl.write("wl-test-results12.txt", True, "scores")
+# wl = Wordlist("wl-test2.txt")
+# wl.write("wl-test2-results4.txt", True, "scores")
+
+wl = Wordlist()
+# wl.add(["doi", "sonofa", "grr", "yadig", "taylorswift", "sanfran", "ohwow", "tryon"])
+wl.add(["flavodoxin", "lyase", "neap", "keepsake"])
+# print(wl, wl[4])
+# wl.write("wl-test3-results1.txt", True, "scores")
