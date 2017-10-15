d538b4ec8bf36747d878238f11c6a5292fc163e5
keiranking <me@keiranking.com>
2017-10-15 01:53:46 -0400

    Can plot with either SP or CT data.

--- plot.py
+++ plot.py
@@ -3,6 +3,7 @@ import json
 import math
 
 ct = json.loads(open("ct-intersect.json.txt","r").read())
+sp = json.loads(open("sp.json.txt","r").read())
 gs = json.loads(open("gs-intersect.json.txt","r").read())
 
 blacklist = open("blacklist.txt", "r").read().split("\n")
@@ -17,19 +18,21 @@ for entry in blacklist:
     if entry:
         l = len(entry)
         blacklist_x.append(math.log10(gs[l][entry]))
-        blacklist_y.append(ct[l][entry])
+        # blacklist_y.append(ct[l][entry])
+        blacklist_y.append(math.log10(int(sp[l][entry])))
 
 for entry in whitelist:
     if entry:
         l = len(entry)
         whitelist_x.append(math.log10(gs[l][entry]))
-        whitelist_y.append(ct[l][entry])
+        # whitelist_y.append(ct[l][entry])
+        whitelist_y.append(math.log10(int(sp[l][entry])))
 
 plt.scatter(whitelist_x, whitelist_y, None, c="blue", alpha=0.4, marker=r'o',
             label="good")
 plt.scatter(blacklist_x, blacklist_y, None, c="red", alpha=0.8, marker=r'o',
             label="bad")
-plt.xlabel("Cultural relevance")
-plt.ylabel("Crossword appearances")
+plt.xlabel("Google hits in the past year, in millions (log scale)")
+plt.ylabel("(SP) Crossword appearances (log scale)")
 # plt.legend(loc=2)
 plt.show()
